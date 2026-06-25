ede4a8c28519f7dc0d8d2ab9477cc21ef1198ef8
Andrew Skrypnyk <a.g.skryp@gmail.com>
2026-06-24 21:37:20 -0400

    header wip

--- wp-content/themes/menekas-writing/header.php
+++ wp-content/themes/menekas-writing/header.php
@@ -1,17 +1,7 @@
-<?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package Meneka\'s_Writing
- */
-
-?>
 <!doctype html>
+
 <html <?php language_attributes(); ?>>
+
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -21,39 +11,50 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'menekas-writing' ); ?></a>
+	<?php wp_body_open(); ?>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$menekas_writing_description = get_bloginfo( 'description', 'display' );
-			if ( $menekas_writing_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $menekas_writing_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+	<div id="page" class="site">
+		<a 
+			class="skip-link screen-reader-text" 
+			href="#primary"
+		><?php esc_html_e( 'Skip to content', 'menekas-writing' ); ?></a>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'menekas-writing' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+		<header id="masthead" class="site-header">
+			<nav class="container">
+				<ul>
+					<li>
+						<?php
+							the_custom_logo();
+						
+							if ( is_front_page() && is_home() ) {
+						?>
+							<h1 class="site-title">
+								<a 
+									href="<?php echo esc_url( home_url( '/' ) ); ?>" 
+									rel="home"
+								><?php bloginfo( 'name' ); ?></a>
+							</h1>
+						<?php
+							} 
+							
+							else {
+						?>
+							<p class="site-title">
+								<a 
+									href="<?php echo esc_url( home_url( '/' ) ); ?>" 
+									rel="home"
+								><?php bloginfo( 'name' ); ?></a>
+							</p>
+						<?php } ?>
+					</li>
+				</ul>
+				
+				<?php
+					wp_nav_menu( array(
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'primary-menu',
+						'container'      => false,
+					) );
+				?>
+			</nav>
+		</header>
