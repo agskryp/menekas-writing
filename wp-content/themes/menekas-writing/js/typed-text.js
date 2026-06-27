8e62b435cf5e1a2a407c895dd0a1fbc1c5edafe4
Andrew Skrypnyk <a.g.skryp@gmail.com>
2026-06-26 23:13:50 -0400

    adjust typed js

--- wp-content/themes/menekas-writing/js/typed-text.js
+++ wp-content/themes/menekas-writing/js/typed-text.js
@@ -4,7 +4,10 @@ import Typed from 'typed.js';
 
 var typed = new Typed('.element', {
   stringsElement: '#typed-strings',
-  typeSpeed: 40,
+  typeSpeed: 66,
+	backSpeed: 46,
+	backDelay: 1000,
+	shouldBackspace: true,
 	loop: true,
 } );
  
