961224a954069b8af4e6a2fa5b6d6682e2ffe06e
Andrew Skrypnyk <a.g.skryp@gmail.com>
2026-07-11 23:32:37 -0400

    Update WordPress, disable comments

--- wp-content/themes/amvs-writing/functions.php
+++ wp-content/themes/amvs-writing/functions.php
@@ -39,9 +39,6 @@ function amvs_writing_setup() {
 		* to output valid HTML5.
 		*/
 	add_theme_support( 'html5', array(
-		'search-form',
-		'comment-form',
-		'comment-list',
 		'gallery',
 		'caption',
 		'style',
